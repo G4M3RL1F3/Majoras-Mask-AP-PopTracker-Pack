f1b297c554b2b5d7b6116f2e79af25c4166572e9
G4M3R L1F3 <patrick-doiron@hotmail.com>
2026-06-03 21:24:52 -0300

    Fixed Woodfall region access rules being incorrect

--- scripts/regions.lua
+++ scripts/regions.lua
@@ -33,7 +33,7 @@ function baby_woodfall()
     return baby_south_swamp() and has("deku")
 end
 function woodfall()
-    return can_use_owl("owl_woodfall") or (south_swamp() and has("deku") and has("soul_deku_flowers"))
+    return south_swamp() and ((has("deku") and has_absurd_soul("soul_deku_flowers")) or can_use_owl("owl_woodfall"))
 end
 
 -- Woodfall -> Woodfall Temple
